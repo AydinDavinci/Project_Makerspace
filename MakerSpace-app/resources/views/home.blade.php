5149a241becb1425ddef8aedbc3728887f03f2c8
AydinDavinci <143701285+AydinDavinci@users.noreply.github.com>
2026-03-20 10:48:18 +0100

    Update home page
    Kleine update aan de home page, tekst veranderd, slogan toegevoeg, en wat scss aanpassingen.

--- MakerSpace-app/resources/views/home.blade.php
+++ MakerSpace-app/resources/views/home.blade.php
@@ -12,7 +12,7 @@
         <section class="main-section" id="home-section">
             <div class="home-header">
                 <img class="Logo" src="{{ asset('images/Logo-large.png') }}" alt="Logo" width="564" height="164">
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit<br> sed do eiusmod tempor,</span>
+                <span>Welkom bij VinciLab! Eigen ontwerp of niet, <br> we hebben wat je zoekt!</span>
                 <div class="home-header__buttons">
                     <button class="button-primary">Login</button><button class="button-secondary">Register</button>
                 </div>
@@ -21,19 +21,23 @@
                 <div class="home-infoblock__image">image</div>
                 <div class="home-infoblock__text">
                     <div>
-                        <h3>Title</h3>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        <h3>Welkom bij VinciLab!</h3>
+                        Via deze website kun je eenvoudig jouw ontwerp uploaden en een printopdracht plaatsen. Kies het materiaal, formaat en de afwerking die het beste bij jouw project past. Ons team zorgt ervoor dat jouw ontwerp nauwkeurig en met zorg wordt geprint.
+                    </div>
                 </div>
             </div>
             <div class="home-infoblock">
-                <div class="home-infoblock__text text-right">
-                    <h3>Title</h3>
+                <div class="home-infoblock__text">
+                    <div class="text-right">
+                        <h3>Eigen ontwerp of niet!</h3>
+                        Heb je geen eigen ontwerp, maar wil je toch een 3D print laten maken? Geen probleem! We hebben een uitgebreide catalogus met kant-en-klare ontwerpen waaruit je kunt kiezen. Of je nu een school project hebt of gewoon iets leuks wilt laten printen, bij VinciLab ben je aan het juiste adres!
+                    </div>
                 </div>
                 <div class="home-infoblock__image">image</div>
             </div>
             <div class="home-ending">
                 <h2>Student bij DaVinci College Dordrecht?</h2>
-                <span>Wil je gebruik maken van de 3D printers en <br>ben je student bij DaVinci College Dordrecht?</span>
+                <span>En wil je gebruik maken van onze 3D printers?</span>
                 <a href="{{ route('register') }}"><button class="button-primary">Vraag je account aan!</button></a>
             </div>
         </section>
