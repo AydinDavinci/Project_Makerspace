b76761287883ab0ebcafd9587f064f0e65e8e8f2
jorisKuinDaVinci <99074486@mydavinci.nl>
2026-04-09 12:02:29 +0200

    controllers en middleware aangemaakt en aangepast

--- MakerSpace-app/app/Http/Controllers/Auth/RegisteredUserController.php
+++ MakerSpace-app/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -35,10 +35,11 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $user = User::create([
+$user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => 'user',
         ]);
 
         event(new Registered($user));
