d93eac234d0877035a2060dbf8fd499a4120005e
jorisKuinDaVinci <99074486@mydavinci.nl>
2026-04-10 16:30:08 +0200

    rollen toegevoegd aan de user.php model
    de rollen die zijn toegevoegd:
    admin
    printoperator
    user

--- MakerSpace-app/app/Models/User.php
+++ MakerSpace-app/app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -43,6 +44,22 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role' => 'string',
         ];
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isPrintOperator(): bool
+    {
+        return $this->role === 'print operator';
+    }
+
+    public function isUser(): bool
+    {
+        return $this->role === 'user';
     }
 }
