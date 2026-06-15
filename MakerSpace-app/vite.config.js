1514100f5983909c6aa1eb3158e3ff421e3f8a86
Michelkroon-bit <143705955+Michelkroon-bit@users.noreply.github.com>
2026-06-15 11:58:58 +0200

    final bug fixes and complete version

--- MakerSpace-app/vite.config.js
+++ MakerSpace-app/vite.config.js
@@ -1,14 +1,31 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import os from 'os';
+
+function getLocalExternalIP() {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return 'localhost';
+}
 
 export default defineConfig({
-    plugins: [
-        laravel([
-            'resources/scss/app.scss',
-            'resources/js/app.js',
-        ]),
-    ],
     server: {
-        host: 'localhost',
+        host: true,
+        port: 5173,
+        hmr: {
+            host: getLocalExternalIP(),
+        },
     },
-})
+    plugins: [
+        laravel({
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
