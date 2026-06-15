4cd5b9200677f43a587b831d0488dd7c9bc2d445
Michelkroon-bit <143705955+Michelkroon-bit@users.noreply.github.com>
2026-06-15 12:18:39 +0200

    quick fix for connection and scss errors

--- MakerSpace-app/vite.config.js
+++ MakerSpace-app/vite.config.js
@@ -1,26 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import os from 'os';
-
-function getLocalExternalIP() {
-    const interfaces = os.networkInterfaces();
-    for (const name of Object.keys(interfaces)) {
-        for (const iface of interfaces[name]) {
-            if (iface.family === 'IPv4' && !iface.internal) {
-                return iface.address;
-            }
-        }
-    }
-    return 'localhost';
-}
 
 export default defineConfig({
     server: {
-        host: true,
+        host: 'localhost',
         port: 5173,
-        hmr: {
-            host: getLocalExternalIP(),
-        },
     },
     plugins: [
         laravel({
